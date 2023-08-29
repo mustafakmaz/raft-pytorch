06098032205c72fa2030b92f9e196e83e4b84403
Mustafa Yasin Akmaz <mustafa.akmaz19@hotmail.com>
2023-08-29 11:48:33 +0300

    added raft_large and raft_small models

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import cv2
-import numpy as np
 import torch
-import torchvision
-from torchvision.models.optical_flow import Raft_Small_Weights
+from torchvision.models.optical_flow import raft_small, Raft_Small_Weights
+from torchvision.models.optical_flow import raft_large, Raft_Large_Weights
 from torchvision.utils import flow_to_image
 from utils import BasicUtils
 
 def frame_preprocess(frame, device):
     frame = torch.from_numpy(frame).permute(2, 0, 1).float()
     frame = frame.unsqueeze(0)
-    frame = frame.to(device)
     return frame
 
-model = torchvision.models.optical_flow.raft_small(weights=Raft_Small_Weights)
-device = BasicUtils().device_chooser
+model = raft_small(weights=Raft_Small_Weights.C_T_V2)
+device = BasicUtils().device_chooser()
 
 model.eval()
-video_path = "/content/woman.mp4"
+video_path = "woman.mp4"
 cap = cv2.VideoCapture(video_path)
 with torch.no_grad():
     while True:
@@ -24,9 +22,7 @@ with torch.no_grad():
         ret2, frame2 = cap.read()
         frame1 = frame_preprocess(frame1, device)
         frame2 = frame_preprocess(frame2, device)
-        # predict the flow
         flows = model(frame1.to(device), frame2.to(device))
-        # transpose the flow output and convert it into numpy array
         flows = flows[-1].detach().cpu()
         print(f"dtype = {flows.dtype}")
         print(f"shape = {flows.shape} = (N, 2, H, W)")
@@ -35,7 +31,8 @@ with torch.no_grad():
         flow_imgs = flow_imgs[0].permute(1, 2, 0)
         flow_imgs = flow_imgs.numpy()
         flow_imgs = cv2.cvtColor(flow_imgs, cv2.COLOR_RGB2BGR)
-        cv2.imshow(flow_imgs)
+        cv2.imshow("OUT",flow_imgs)
+        cv2.waitKey(1)
 
         if not ret2:
             cap.release()
